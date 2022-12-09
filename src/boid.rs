1ed52c97795d4c76307e0bc957f332b607c0e5a1
Felipe Balbi <felipe@balbi.sh>
2022-12-09 12:40:24 +0200

    boid: add boid module
    This new module encapsulates details of the boid simulation.
    
    Signed-off-by: Felipe Balbi <felipe@balbi.sh>

---
+++ src/boid.rs
@@ -0,0 +1,174 @@
+use micromath::vector::{F32x2, Vector};
+use uefi::proto::console::gop::{BltOp, BltPixel, GraphicsOutput};
+use uefi::Result;
+
+#[derive(PartialEq, Clone)]
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+
+#[derive(PartialEq, Clone)]
+pub struct Boid {
+    body: Rectangle,
+    position: F32x2,
+    velocity: F32x2,
+    acceleration: F32x2,
+}
+
+const MAX_FORCE: f32 = 0.2;
+const MAX_VELOCITY: f32 = 5.0;
+
+impl Boid {
+    pub fn new(position: F32x2, velocity: F32x2) -> Self {
+        Self {
+            body: Rectangle {
+                width: 5.0,
+                height: 5.0,
+            },
+            acceleration: Default::default(),
+            position,
+            velocity,
+        }
+    }
+
+    fn edges(&mut self, width: usize, height: usize) {
+        if (self.position.x + self.body.width) >= width as f32 {
+            self.position.x = self.body.width;
+        } else if (self.position.x) <= 0.0 {
+            self.position.x = width as f32 - self.body.width;
+        }
+
+        if (self.position.y + self.body.height) >= height as f32 {
+            self.position.y = self.body.height;
+        } else if (self.position.y) <= self.body.height {
+            self.position.y = height as f32 - self.body.height;
+        }
+    }
+
+    fn alignment(&self, boids: &[Boid]) -> F32x2 {
+        let perception_radius = 25.0;
+        let mut steering = F32x2::default();
+        let mut total = 0.0;
+
+        for boid in boids.iter() {
+            let d = self.position.distance(boid.position);
+
+            if boid != self && d < perception_radius {
+                steering += boid.velocity;
+                total += 1.0;
+            }
+        }
+
+        if total > 0.0 {
+            steering *= 1.0 / total;
+            let mag = steering.magnitude();
+            steering *= MAX_VELOCITY / mag;
+            steering -= self.velocity;
+
+            let mag = steering.magnitude();
+            if mag > MAX_FORCE {
+                steering *= MAX_FORCE / mag;
+            }
+        }
+
+        steering
+    }
+
+    fn separation(&self, boids: &[Boid]) -> F32x2 {
+        let perception_radius = 24.0;
+        let mut steering = F32x2::default();
+        let mut total = 0.0;
+
+        for boid in boids.iter() {
+            let d = self.position.distance(boid.position);
+
+            if boid != self && d < perception_radius {
+                let mut diff = self.position - boid.position;
+                diff *= 1.0 / (d * d);
+                steering += diff;
+                total += 1.0;
+            }
+        }
+
+        if total > 0.0 {
+            steering *= 1.0 / total;
+            let mag = steering.magnitude();
+            steering *= MAX_VELOCITY / mag;
+            steering -= self.velocity;
+
+            let mag = steering.magnitude();
+            if mag > MAX_FORCE {
+                steering *= MAX_FORCE / mag;
+            }
+        }
+
+        steering
+    }
+
+    fn cohesion(&self, boids: &[Boid]) -> F32x2 {
+        let perception_radius = 50.0;
+        let mut steering = F32x2::default();
+        let mut total = 0.0;
+
+        for boid in boids.iter() {
+            let d = self.position.distance(boid.position);
+
+            if boid != self && d < perception_radius {
+                steering += boid.position;
+                total += 1.0;
+            }
+        }
+
+        if total > 0.0 {
+            steering *= 1.0 / total;
+            steering -= self.position;
+            let mag = steering.magnitude();
+            steering *= MAX_VELOCITY / mag;
+            steering -= self.velocity;
+
+            let mag = steering.magnitude();
+            if mag > MAX_FORCE {
+                steering *= MAX_FORCE / mag;
+            }
+        }
+
+        steering
+    }
+
+    pub fn flock(&mut self, boids: &[Boid]) {
+        let alignment = self.alignment(boids) * 1.5;
+        let cohesion = self.cohesion(boids) * 1.0;
+        let separation = self.separation(boids) * 2.0;
+
+        self.acceleration += alignment;
+        self.acceleration += cohesion;
+        self.acceleration += separation;
+    }
+
+    pub fn update(&mut self, width: usize, height: usize) {
+        self.position += self.velocity;
+        self.velocity += self.acceleration;
+
+        let mag = self.velocity.magnitude();
+        if mag > MAX_VELOCITY {
+            self.velocity *= MAX_VELOCITY / mag;
+        }
+
+        self.acceleration *= 0.0;
+
+        self.edges(width, height);
+    }
+
+    pub fn draw(&mut self, gop: &mut GraphicsOutput) -> Result {
+        let color = BltPixel::new(255, 0, 0);
+
+        gop.blt(BltOp::VideoFill {
+            color,
+            dest: (self.position.x as usize, self.position.y as usize),
+            dims: (self.body.width as usize, self.body.height as usize),
+        })?;
+
+        Ok(())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,185 +3,17 @@
 
 extern crate alloc;
 
+mod boid;
+
+use crate::boid::Boid;
 use alloc::vec::Vec;
 use core::mem;
 use log::debug;
-use micromath::vector::{F32x2, Vector};
+use micromath::vector::F32x2;
 use uefi::proto::console::gop::{BltOp, BltPixel, GraphicsOutput};
 use uefi::proto::rng::Rng;
 use uefi::table::boot::BootServices;
 use uefi::Result;
-
-#[derive(PartialEq, Clone)]
-struct Rectangle {
-    width: f32,
-    height: f32,
-}
-
-#[derive(PartialEq, Clone)]
-struct Boid {
-    body: Rectangle,
-    position: F32x2,
-    velocity: F32x2,
-    acceleration: F32x2,
-}
-
-const MAX_FORCE: f32 = 0.2;
-const MAX_VELOCITY: f32 = 5.0;
-
-impl Boid {
-    fn new(position: F32x2, velocity: F32x2) -> Self {
-        Self {
-            body: Rectangle {
-                width: 5.0,
-                height: 5.0,
-            },
-            acceleration: Default::default(),
-            position,
-            velocity,
-        }
-    }
-
-    fn edges(&mut self, width: usize, height: usize) {
-        if (self.position.x + self.body.width) >= width as f32 {
-            self.position.x = self.body.width;
-        } else if (self.position.x) <= 0.0 {
-            self.position.x = width as f32 - self.body.width;
-        }
-
-        if (self.position.y + self.body.height) >= height as f32 {
-            self.position.y = self.body.height;
-        } else if (self.position.y) <= self.body.height {
-            self.position.y = height as f32 - self.body.height;
-        }
-    }
-
-    fn alignment(&self, boids: &[Boid]) -> F32x2 {
-        let perception_radius = 25.0;
-        let mut steering = F32x2::default();
-        let mut total = 0.0;
-
-        for boid in boids.iter() {
-            let d = self.position.distance(boid.position);
-
-            if boid != self && d < perception_radius {
-                steering += boid.velocity;
-                total += 1.0;
-            }
-        }
-
-        if total > 0.0 {
-            steering *= 1.0 / total;
-            let mag = steering.magnitude();
-            steering *= MAX_VELOCITY / mag;
-            steering -= self.velocity;
-
-            let mag = steering.magnitude();
-            if mag > MAX_FORCE {
-                steering *= MAX_FORCE / mag;
-            }
-        }
-
-        steering
-    }
-
-    fn separation(&self, boids: &[Boid]) -> F32x2 {
-        let perception_radius = 24.0;
-        let mut steering = F32x2::default();
-        let mut total = 0.0;
-
-        for boid in boids.iter() {
-            let d = self.position.distance(boid.position);
-
-            if boid != self && d < perception_radius {
-                let mut diff = self.position - boid.position;
-                diff *= 1.0 / (d * d);
-                steering += diff;
-                total += 1.0;
-            }
-        }
-
-        if total > 0.0 {
-            steering *= 1.0 / total;
-            let mag = steering.magnitude();
-            steering *= MAX_VELOCITY / mag;
-            steering -= self.velocity;
-
-            let mag = steering.magnitude();
-            if mag > MAX_FORCE {
-                steering *= MAX_FORCE / mag;
-            }
-        }
-
-        steering
-    }
-
-    fn cohesion(&self, boids: &[Boid]) -> F32x2 {
-        let perception_radius = 50.0;
-        let mut steering = F32x2::default();
-        let mut total = 0.0;
-
-        for boid in boids.iter() {
-            let d = self.position.distance(boid.position);
-
-            if boid != self && d < perception_radius {
-                steering += boid.position;
-                total += 1.0;
-            }
-        }
-
-        if total > 0.0 {
-            steering *= 1.0 / total;
-            steering -= self.position;
-            let mag = steering.magnitude();
-            steering *= MAX_VELOCITY / mag;
-            steering -= self.velocity;
-
-            let mag = steering.magnitude();
-            if mag > MAX_FORCE {
-                steering *= MAX_FORCE / mag;
-            }
-        }
-
-        steering
-    }
-
-    fn flock(&mut self, boids: &[Boid]) {
-        let alignment = self.alignment(boids) * 1.5;
-        let cohesion = self.cohesion(boids) * 1.0;
-        let separation = self.separation(boids) * 2.0;
-
-        self.acceleration += alignment;
-        self.acceleration += cohesion;
-        self.acceleration += separation;
-    }
-
-    fn update(&mut self, width: usize, height: usize) {
-        self.position += self.velocity;
-        self.velocity += self.acceleration;
-
-        let mag = self.velocity.magnitude();
-        if mag > MAX_VELOCITY {
-            self.velocity *= MAX_VELOCITY / mag;
-        }
-
-        self.acceleration *= 0.0;
-
-        self.edges(width, height);
-    }
-
-    fn draw(&mut self, gop: &mut GraphicsOutput) -> Result {
-        let color = BltPixel::new(255, 0, 0);
-
-        gop.blt(BltOp::VideoFill {
-            color,
-            dest: (self.position.x as usize, self.position.y as usize),
-            dims: (self.body.width as usize, self.body.height as usize),
-        })?;
-
-        Ok(())
-    }
-}
 
 pub fn run(bt: &BootServices) -> Result {
     debug!("Starting Boid Simulation");
@@ -247,8 +79,6 @@ fn get_random_usize(rng: &mut Rng) -> usize {
 }
 
 fn get_random_usize_range(rng: &mut Rng, start: usize, end: usize) -> usize {
-    let mut buf = [0; mem::size_of::<usize>()];
-    rng.get_rng(None, &mut buf).expect("get_rng failed");
-    let number = usize::from_le_bytes(buf);
+    let number = get_random_usize(rng);
     (number % (end - start + 1)) + start
 }
